a5665c6e64bae57561ea15bdcbeaf6d7881871ba
Angel Umeh <akcumeh@gmail.com>
2026-07-13 16:07:56 +0100

    chore: remove stale js

--- src/ui/toast.js
+++
@@ -1,8 +0,0 @@
-let tTimer;
-export function showToast(msg, col = '#84cc16') {
-    clearTimeout(tTimer);
-    document.getElementById('ttxt').textContent = msg;
-    document.getElementById('tpip').style.background = col;
-    document.getElementById('toast').classList.add('show');
-    tTimer = setTimeout(() => document.getElementById('toast').classList.remove('show'), 2600);
-}
